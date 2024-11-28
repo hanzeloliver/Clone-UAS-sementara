4778efb6b0989030e2f373cd058e80c58de74f38
hanzeloliver <hanzel.oliver55@gmail.com>
2024-11-28 08:38:56 +0700

    Penambahan codingan

--- UAS-Project/routes/web.php
+++ UAS-Project/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TentangSMKController;
+use App\Http\Controllers\PembelajaranController;
+use App\Http\Controllers\SekolahKamiController;
+use App\Http\Controllers\PendaftaranController;
+use App\Http\Controllers\KehidupanSiswaController;
+use App\Http\Controllers\KarierController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tentang-smk', [TentangSMKController::class, 'index'])->name('tentang-smk');
+Route::get('/pembelajaran', [PembelajaranController::class, 'index'])->name('pembelajaran');
+Route::get('/sekolah-kami', [SekolahKamiController::class, 'index'])->name('sekolah-kami');
+Route::get('/pendaftaran', [PendaftaranController::class, 'index'])->name('pendaftaran');
+Route::get('/kehidupan-siswa', [KehidupanSiswaController::class, 'index'])->name('kehidupan-siswa');
+Route::get('/karier', [KarierController::class, 'index'])->name('karier');

5ddbe15f723fbd6d06d7bf0594916529a30f636c
MG42freak <mg42freak@gmail.com>
2024-12-09 22:27:04 +0700

    Update head.blade.php

--- UAS-Project/resources/views/partials/head.blade.php
+++ UAS-Project/resources/views/partials/head.blade.php
@@ -2,5 +2,5 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="/UAS-Project/resources/css/app.css">
 </head>

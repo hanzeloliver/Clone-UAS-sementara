81412bb7de33c5e7dfbe99921f8a8a17d167fd81
MG42freak <mg42freak@gmail.com>
2024-12-11 22:01:29 +0700

    Update footer.blade.php

--- UAS-Project/resources/views/partials/footer.blade.php
+++ UAS-Project/resources/views/partials/footer.blade.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <p>Universitas Lorem Ipsum</p>
-                <p>Jl. Ipsum Boulevard, Gading Ipsum, Kel. Curug Ipsum, Kec. Ipsum Dua, Kab. Ipsum, Prop. Ipsum 12345, Indonesia</p>
+                <p>SMK INFORMATIKA DASANA INDAH</p>
+                <p>Alamat: </p>
             </div>
             <div class="col-md-6">
                 <p><strong>Contact Information</strong></p>
-                <p>(T) +62-21.1234.5678</p>
-                <p>(F) +62-21.8765.4321</p>
+                <p>(T) +62-8xx-xxxx-xxxx</p>
+                <p>(F) +62-8xx-xxxx-xxxx</p>
                 <p>e-mail: lorem@ipsum.com</p>
             </div>
         </div>
